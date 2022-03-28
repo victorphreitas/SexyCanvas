4e15c21ac3e36dc89cc028d99dd2a30d12db2ed9
Victor Freitas <97434584+victorphreitas@users.noreply.github.com>
2022-03-28 11:31:13 +0200

    Responsive version

--- main.js
+++ main.js
@@ -5,6 +5,26 @@ const innerChildBtns = document.querySelectorAll(".container-child-buttons butto
 const textAreas = document.querySelectorAll("textarea");
 let checkButton;
 
+//explanation object goes here
+const explanationObj = {
+  greed: ["explanationGreed ", "example1Greed"],
+  lust: ["explanationLust", "example1Lust"],
+  pride: ["explanationPride", "example1Pride"],
+  envy: ["explanationEnvy", "example1Envy"],
+  sloth: ["explanationSloth", "example1Sloth"],
+  wrath: ["explanationWrath", "example1Wrath"],
+  gluttony: ["explanationGluttony", "example1Gluttony"],
+  innerChild: ["explanationInnerChild", "example1InnerChild"],
+  curiosity: ["explanationCuriosity", "example1Curiosity"],
+  freedom: ["explanationFreedom", "example1Freedom"],
+  love: ["explanationLove", "example1Love"],
+  spirituality: ["explanationSpirituality", "example1Spitituality"],
+  fun: ["explanationFun", "example1Fun"],
+  safety: ["explanationSafety", "example1Safety"],
+  reward: ["explanationReward", "example1Reward"],
+  belonging: ["explanationBelongin", "example1Belonging"]
+}
+
 
 //event lisnteners to the sin buttons
 sinBtns.forEach((button)=>{
@@ -18,7 +38,7 @@ innerChildBtns.forEach((button)=>{
   button.addEventListener("click", explain);
 })
 
-
+//Email js library goes here
 window.onload = function() {
   document.getElementById('template_fys9v8f').addEventListener('submit', function(event) {
       event.preventDefault();
@@ -39,83 +59,149 @@ window.onload = function() {
   });
 }
 
+//to swing between examples and explanations when you click the same button again
+let tellEx = true
 
 
 function explain(event) {
    if (event.target.parentElement.className === "container-main-buttons" && event.target.textContent !== "Enviar >>>"){
      if (event.target.textContent === "ganancia"){
-       const gananciaExp = "Ganancia esta expliacada aqui!"
-       document.querySelector("#explanation").innerText = gananciaExp
+       if (tellEx === true){
+        document.querySelector("#explanation").innerText = explanationObj.greed[0]
+        tellEx = false
+       } else {
+        document.querySelector("#explanation").innerText = explanationObj.greed[1]
+        tellEx = true
+       }
        document.querySelector("textarea[name=message2]").focus()
      } else if (event.target.textContent === "client"){
       const clientExp = "Client esta expliacada aqui!"
       document.querySelector("#explanation").innerText = clientExp
       document.querySelector("textarea[name=message1]").focus()
      } else if (event.target.textContent === "luxuria"){
-      const luxuriaExp = "Luxuria esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = luxuriaExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.lust[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.lust[1]
+        tellEx = true
+      }
       document.querySelector("textarea[name=message3]").focus()
      } else if (event.target.textContent === "vaidade"){
-      const vaidadeExp = "Vaidade esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = vaidadeExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.pride[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.pride[1]
+        tellEx = true
+      }
       document.querySelector("textarea[name=message4]").focus()
      } else if (event.target.textContent === "inveja"){
-      const invejaExp = "Inveja esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = invejaExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.envy[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.envy[1]
+        tellEx = true
+      }
       document.querySelector("textarea[name=message5]").focus()
      } else if (event.target.textContent === "preguica"){
-      const preguicaExp = "Preguica esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = preguicaExp 
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.sloth[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.sloth[1]
+        tellEx = true
+      } 
       document.querySelector("textarea[name=message6]").focus()
      } else if (event.target.textContent === "gula"){
-      const gulaExp = "Gula esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = gulaExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.gluttony[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.gluttony[1]
+        tellEx = true
+      }
       document.querySelector("textarea[name=message8]").focus()
      } else if (event.target.textContent === "ira"){
-      const iraExp = "Ira esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = iraExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.wrath[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.wrath[1]
+        tellEx = true
+      }
       document.querySelector("textarea[name=message7]").focus()
      }
    } else if (event.target.parentElement.className === "container-child-buttons"){
     if (event.target.textContent === "curiosidade"){
       const curiosidadeExp = "Curiosidade esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = curiosidadeExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.curiosity[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.curiosity[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "liberdade"){
-      const liberdadeExp = "Liberdade esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = liberdadeExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.freedom[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.freedom[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "amor"){
-      const amorExp = "Amor esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = amorExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.love[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.love[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "espiritualidade"){
-      const espiritualidadeExp = "Espiritualidade esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = espiritualidadeExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.spirituality[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.spirituality[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "diversao"){
-      const diversaoExp = "Diversao esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = diversaoExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.fun[0] 
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.fun[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "seguranca"){
-      const segurancaExp = "Seguranca esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = segurancaExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.safety[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.safety[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "recompensa"){
-      const recompensaExp = "Recompensa esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = recompensaExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.reward[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.reward[1]
+        tellEx = true
+      }
     } else if (event.target.textContent === "pertencimento"){
-      const pertencimentoExp = "Pertencimento esta expliacada aqui!"
-      document.querySelector("#explanation").innerText = pertencimentoExp
+      if (tellEx === true) {
+        document.querySelector("#explanation").innerText = explanationObj.belonging[0]
+        tellEx = false
+      } else {
+        document.querySelector("#explanation").innerText = explanationObj.belonging[1]
+        tellEx = true
+      }
     }
     document.querySelector("textarea[name=message9]").focus() 
    }
   event.preventDefault();
 } 
 
-
-
-
-
-let val;
-
-val = container;
-val = sinBtns;
-val = innerChildBtns;
-val = textAreas;
-
-// console.log(val)
